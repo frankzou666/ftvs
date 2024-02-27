1637c7b402d01eea1579f2ee381781a657d1d9d0
oliver <overl@local.com>
2024-02-27 10:51:32 +0800

    移动web,学习rem和less

---
+++ v3ts/v3tsvite/v3tsvite/src/components/Home.vue
@@ -0,0 +1,60 @@
+<template>
+<div>
+   <p>my name is {{ name }}</p>
+   <button @click="updateCount">count</button>
+
+   <p>nameFromData from {{ nameFromData }}</p>
+   
+   <button @click="updatenameFromData">nameFromData</button>
+
+
+</div>
+</template>
+
+<script  lang="ts">
+ import { defineComponent, ref} from 'vue';
+ export default defineComponent ({
+     name :'Home',
+     //setup新的选项函数，返回一个对像，可以在模版中直接使用
+     //只在初始化时执行一次
+     //props参数，获取到是父级组件向子组件专来过数据
+     setup(props,context) {
+         console.log(props.age)
+         //和props取到的值有什么区别？
+         console.log(context.attrs.age)
+        let name = ref(1);
+        // let name = reactive({
+        //     name:1
+        // })
+        function updateCount() {
+            name.value++;
+            console.log('good')
+            console.log(name)
+
+        };
+        return {
+            name,updateCount
+        }
+     },
+     //可以同时设置data和setup属性
+     data () {
+        return {nameFromData: 1}
+     },
+     methods:{
+        updatenameFromData() {
+            //可以访问reactive()中定义的属性
+            //return this.name++;
+            return this.nameFromData++;
+
+        },
+     }
+ })
+
+
+
+</script>
+<style scoped>
+
+
+
+</style>
---
+++ v3ts/v3tsvite/v3tsvite/src/main.ts
@@ -0,0 +1,12 @@
+
+//vu3使用createApp函数来创建vm实例
+import { createApp } from 'vue'
+
+//引入css
+import './style.css'
+
+//引入App组件
+import App from './App.vue'
+
+//创建对像，并加载到一个index.html元素
+createApp(App).mount('#app')
